543c94d02a9ba43ea7cbef9fa2e965f97b1acced
jon <jrayvid@gmail.com>
2016-02-08 15:28:56 -0500

    added paperclip to user edit page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204230059) do
+ActiveRecord::Schema.define(version: 20160208200248) do
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20160204230059) do
     t.string   "email"
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
