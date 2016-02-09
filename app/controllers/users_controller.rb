1b26cf415a0547d2a7c102d01691fe54634103f8
John LaGreco <johnlagreco@gmail.com>
2016-02-09 17:48:45 -0500

    Added search page, function and styling.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
     	redirect_to '/' 
   end
 
+  def search
+    @users = User.where(roll: params[:query])
+  end
+
   private
   def user_params
   params.require(:user).permit(:fname, :lname, :city, :state, :email, :username, :password, :roll, :avatar)
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   delete '/logout' => 'sessions#destroy'
 
+  get '/search' => 'users#search'
+
   # post 'users/new' => 'user#show'
   
   # The priority is based upon order of creation: first created -> highest priority.
